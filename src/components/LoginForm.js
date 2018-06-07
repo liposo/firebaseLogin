de57d79bb16821d8e73d7191169323efa6ce9183
felipe <felipezb@live.com>
2018-06-07 13:28:16 -0300

    input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { TextInput } from "react-native";
 import { CardSection, Card, Button } from "./common";
 
 class LoginForm extends Component {
+  // Create state for the component
+  state = {
+    text: ""
+  };
+
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <TextInput
+            underlineColorAndroid="transparent"
+            style={{ flexGrow: 1, height: 40 }}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
         <CardSection />
         <CardSection>
           <Button>Login</Button>
