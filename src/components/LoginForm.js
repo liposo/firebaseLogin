ca1ba78302d4c6d4069d08dcdf816a023dd36b33
felipe <felipezb@live.com>
2018-06-07 10:09:59 -0300

    login form boilerplate code

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { CardSection, Card, Button } from "./common";
 
 class LoginForm extends Component {
   render() {
-    return <View />;
+    return (
+      <Card>
+        <CardSection />
+        <CardSection />
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+      </Card>
+    );
   }
 }
 
