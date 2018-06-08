7e1b0a00b150e8e9faf32530d395aab0bc7c574c
felipe <felipezb@live.com>
2018-06-08 09:58:01 -0300

    back

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React, { Component } from "react";
 import { View } from "react-native";
 
 import firebase from "firebase";
+import config from "./config/firebaseAPIKey";
 
 import { Header } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   initializeFirebase() {
-    var config = {
-      apiKey: "AIzaSyDi4fHL-i5apQs8FWesu64KZkB-ApFrioU",
-      authDomain: "login-819e4.firebaseapp.com",
-      databaseURL: "https://login-819e4.firebaseio.com",
-      projectId: "login-819e4",
-      storageBucket: "login-819e4.appspot.com",
-      messagingSenderId: "1064614059262"
-    };
     firebase.initializeApp(config);
   }
 
