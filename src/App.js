8562edb79d9d89838e4572ede310999447bb642f
felipe <felipezb@live.com>
2018-06-08 16:09:13 -0300

    visual feedback

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { View, Text } from "react-native";
 import firebase from "firebase";
 import config from "./config/firebaseAPIKey";
 
-import { Header, Button, Card, CardSection } from "./components/common";
+import {
+  Header,
+  Button,
+  Card,
+  CardSection,
+  Spinner
+} from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   state = {
-    loggedIn: false,
+    loggedIn: null,
     user: null
   };
 
   componentWillMount() {
     firebase.initializeApp(config);
-
     firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
       user
         ? this.setState({ loggedIn: true, user: firebase.auth().currentUser })
         : this.setState({ loggedIn: false, user: null });
@@ -24,25 +30,35 @@ class App extends Component {
   }
 
   onButtonPress() {
-    console.log(this.state.user);
     firebase.auth().signOut();
   }
 
   renderContent() {
-    return this.state.loggedIn ? (
-      <Card>
-        <CardSection>
-          <Text style={{ fontSize: 20, alignSelf: "center" }}>
-            Logged user: {this.state.user.displayName}
-          </Text>
-        </CardSection>
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Logout</Button>
-        </CardSection>
-      </Card>
-    ) : (
-      <LoginForm />
-    );
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Text style={{ fontSize: 20, alignSelf: "center" }}>
+                Logged user: {this.state.user.displayName}
+              </Text>
+            </CardSection>
+            <CardSection>
+              <Button onPress={this.onButtonPress.bind(this)}>Logout</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner size="large" />
+            </CardSection>
+          </Card>
+        );
+    }
   }
 
   render() {
